5b0e567105210ff2dfc7460ce13af7f1c989b28f
Henry Ong <67896884+100sonly@users.noreply.github.com>
2024-03-07 14:25:38 +0800

    Create DailyChallengeActivity.java

---
+++ app/src/main/java/com/example/visionfitv2/DailyChallengeActivity.java
@@ -0,0 +1,41 @@
+package com.example.visionfitv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class DailyChallangeActivity extends AppCompatActivity{
+    public int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_challange);
+        Button startButton = (Button) findViewById(R.id.startButton);
+        TextView timer = (TextView) findViewById(R.id.timer);
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                new CountDownTimer(3000, 1000){
+                    public void onTick(long millisUntilFinished){
+
+                        timer.setText(String.valueOf(counter));
+                        counter--;
+                    }
+                    public  void onFinish(){
+                        timer.setText("FINISH!!");
+                    }
+                }.start();
+            }
+        });
+    }
+}

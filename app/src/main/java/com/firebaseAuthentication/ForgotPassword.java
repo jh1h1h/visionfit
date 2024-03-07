8c1cc1049195d0babe44ae906f0985af5725dbee
Jae <gohyishen777@gmail.com>
2024-03-07 17:38:24 +0800

    Commented out edgar's lines temporarily

--- app/src/main/java/com/firebaseAuthentication/ForgotPassword.java
+++ app/src/main/java/com/firebaseAuthentication/ForgotPassword.java
@@ -1,21 +1,21 @@
-package com.firebaseAuthentication;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-
-public class ForgotPassword extends AppCompatActivity {
-    
-}
+//package com.firebaseAuthentication;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.text.TextUtils;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ProgressBar;
+//import android.widget.Toast;
+//
+//import com.google.android.gms.tasks.OnFailureListener;
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.firebase.auth.FirebaseAuth;
+//
+//public class ForgotPassword extends AppCompatActivity {
+//
+//}

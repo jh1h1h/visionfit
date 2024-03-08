6e2fd9c819fbeadb9c44c361ca1e66e4aa9c0ad9
edgaraw29@gmail.com <edgaraw29@gmail.com>
2024-03-09 01:09:25 +0800

    feature:forgot password successful

--- app/src/main/java/com/teamten/visionfit/java/balls2/SplashActivity.java
+++ app/src/main/java/com/teamten/visionfit/java/balls2/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, com.firebaseAuthentication.Login.class);
                 startActivity(intent);
                 finish();
             }

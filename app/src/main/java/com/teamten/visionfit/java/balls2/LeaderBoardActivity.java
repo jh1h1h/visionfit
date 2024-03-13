9dbb3b4ebcf9b707b111a93c55b456bd563dd1ae
edgaraw29@gmail.com <edgaraw29@gmail.com>
2024-03-14 00:00:15 +0800

    feature:updated leaderboard and rewards UI

--- app/src/main/java/com/teamten/visionfit/java/balls2/LeaderBoardActivity.java
+++ app/src/main/java/com/teamten/visionfit/java/balls2/LeaderBoardActivity.java
@@ -3,6 +3,7 @@ package com.teamten.visionfit.java.balls2;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.daimajia.androidanimations.library.Techniques;
@@ -15,7 +16,9 @@ public class LeaderBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
-        TextView tv=findViewById(R.id.leaderboardTitle);
+        /*
+         TextView tv=findViewById(R.id.leaderboardTitle);
         YoYo.with(Techniques.SlideInRight).duration(2000).playOn(tv);
+        */
     }
 }

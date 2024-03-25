24d099005d7a10eded1e14251cfa92387ef374c3
edgaraw29@gmail.com <edgaraw29@gmail.com>
2024-03-25 17:45:19 +0800

    feature: FCM push notif by default

---
+++ app/src/main/java/Team10_VisionFit/Backend/firebaseCloudFireStore/PushNotificationService.java
@@ -0,0 +1,34 @@
+package Team10_VisionFit.Backend.firebaseCloudFireStore;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.teamten.visionfit.R;
+
+public class PushNotificationService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        String title = remoteMessage.getNotification().getTitle();
+        String text = remoteMessage.getNotification().getBody();
+        final String CHANNEL_ID = "HEADS_UP_NOTIFICATION";
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                "Heads up Notification",
+                NotificationManager.IMPORTANCE_HIGH
+        );
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        Notification.Builder notification = new Notification.Builder(this, CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setSmallIcon(R.mipmap.visionfitlogo_round)
+                .setAutoCancel(true);
+        NotificationManagerCompat.from(this).notify(1, notification.build());
+        super.onMessageReceived(remoteMessage);
+    }
+}
